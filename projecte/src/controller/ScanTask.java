51f0a43531ad09b7936990ea117836e194e1d37e
HamzaEnti <hamza.elhaddad@enti.cat>
2025-12-27 20:47:04 +0100

    Add files via upload

--- projecte/src/controller/ScanTask.java
+++ projecte/src/controller/ScanTask.java
@@ -2,30 +2,27 @@ package controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.SwingUtilities;
+import javax.swing.SwingUtilities; // necessari per tocar la interficie
 
 import model.ResultatHost;
 import utils.NetworkUtil;
-import view.MainFrame; 
+import view.MainFrame; // la vista de l'oscar
 
 public class ScanTask implements Runnable {
 
     private String ip;
     private MainFrame vista; // variable per guardar la finestra
 
-
-    public ScanTask(String ip, MainFrame vista) {
+    // constructor actualitzat
+    public ScanTask(String ip, MainFrame v) {
         this.ip = ip;
-        this.vista = vista;
+        this.vista = v;
     }
 
     @Override
     public void run() {
-        // primer miro si hi ha ping (timeout 200ms)
         if (NetworkUtil.isReachable(ip, 200)) {
             
-            // si respon creo l'objecte
             ResultatHost host = new ResultatHost(ip);
             host.setEsViu(true);
             
@@ -33,28 +30,23 @@ public class ScanTask implements Runnable {
             // 21:ftp, 22:ssh, 23:telnet, 80:web, 443:https
             // 3306:mysql, 5432:postgres, 3389:escriptori remot windows, 8080:tomcat/web
             int[] llistaPorts = {21, 22, 23, 80, 443, 3306, 3389, 5432, 8080};
-            
             List<Integer> portsTrobats = new ArrayList<>();
 
-            // provo un per un amb la utilitat del nico
             for (int port : llistaPorts) {
-                // li poso 200ms de timeout, si poses mes anira mes lent
                 if (NetworkUtil.isPortOpen(ip, port, 200)) {
                     portsTrobats.add(port);
                 }
             }
             
-            // els guardo
             host.setPortsOberts(portsTrobats);
-
 
             // ZONA VISUAL: avisem a la pantalla que hem trobat algo
             // es fa amb invokeLater perque swing no peti amb els fils
             SwingUtilities.invokeLater(() -> {
                 vista.afegirResultat(host);
             });
-
-            System.out.println("host trobat: " + host.toString());
+            
+            System.out.println("trobat: " + ip);
         }
     }
 }
